12cf37c9d4ffac3ef27f9ede7f0e20adc5a3822a
Stuart Gray <mutinyuxpx@gmail.com>
2019-02-03 12:53:35 -0500

    create wp-config.php
    just some additions to add to wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,15 @@
+/**
+ *  - m00tiny
+ */
+define( 'WP_SITEURL', 'https://www.nutitrionscholar.com' );
+define( 'WP_POST_REVISIONS', 5 );
+define( 'COOKIE_DOMAIN', 'www.nutritionscholar.com' );
+define( 'NOBLOGREDIRECT', 'http://www.nutritionscholar.com' );
+define( 'WP_MEMORY_LIMIT', '256M' );
+define( 'WP_CACHE', true );
+define( 'EMPTY_TRASH_DAYS', 90 );
+// define( 'IMAGE_EDIT_OVERWRITE', true );
+
+
+// FOR FIXING CORRUPT DBS! - m00tiny
+// define( 'WP_ALLOW_REPAIR', true );
