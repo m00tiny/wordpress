7250fec0972646cbb4e3f21d34c75ccbb3a7ca83
Stuart Gray <mutinyuxpx@gmail.com>
2019-02-05 12:11:19 -0500

    Update wp-config.php
    pulled client website address

--- wp-config.php
+++ wp-config.php
@@ -1,10 +1,10 @@
 /**
  *  - m00tiny
  */
-define( 'WP_SITEURL', 'https://www.nutitrionscholar.com' );
+define( 'WP_SITEURL', '' );
 define( 'WP_POST_REVISIONS', 5 );
-define( 'COOKIE_DOMAIN', 'www.nutritionscholar.com' );
-define( 'NOBLOGREDIRECT', 'http://www.nutritionscholar.com' );
+define( 'COOKIE_DOMAIN', 'nohttporprotocol' );
+define( 'NOBLOGREDIRECT', '' );
 define( 'WP_MEMORY_LIMIT', '256M' );
 define( 'WP_CACHE', true );
 define( 'EMPTY_TRASH_DAYS', 90 );
